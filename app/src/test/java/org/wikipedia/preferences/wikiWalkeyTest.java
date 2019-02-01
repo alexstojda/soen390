55262d175e28b44318df2ac7ae1673f6b933a220
EvanDime <evan-dimopoulos@hotmail.com>
2019-02-01 10:57:28 -0500

    [24] used all lower-case for preference keys used assertFalse for test

--- app/src/test/java/org/wikipedia/preferences/wikiWalkeyTest.java
+++ app/src/test/java/org/wikipedia/preferences/wikiWalkeyTest.java
@@ -3,19 +3,14 @@ package org.wikipedia.preferences;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-import org.wikipedia.dataclient.WikiSite;
 import org.wikipedia.settings.Prefs;
-
-import java.util.ArrayList;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
 
 /** Unit tests for Page. */
 @RunWith(RobolectricTestRunner.class)
 public class wikiWalkeyTest {
     @Test
     public void testWikiwalkeyPreferencesDefault() throws Throwable {
-        assertThat(Prefs.isWikiWalkingEnabled(),is(false));
+        assertFalse(Prefs.isWikiWalkingEnabled());
     }
 }
