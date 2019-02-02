92c8e6857f99bc61f2729611727674964b8caa6c
NicoChampagne <nicolas.b.champagne@gmail.com>
2019-02-02 14:28:07 -0500

    [#15] Changes to pass codacy test, again

--- app/src/test/java/org/wikipedia/preferences/WikiWalkeyTest.java
+++ app/src/test/java/org/wikipedia/preferences/WikiWalkeyTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.wikipedia.settings.Prefs;
+
 import static org.junit.Assert.assertFalse;
 
 /** Unit tests for Page. */
