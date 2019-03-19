ba4491a62d108ad20d80ba86de8f3be3c9b0c764
martin <martinas95@yahoo.fr>
2019-03-19 18:17:13 -0400

    (#42) Fixed naming

--- app/src/test/java/org/wikipedia/distractionfree/DistractionFreeTest.java
+++ app/src/test/java/org/wikipedia/distractionfree/DistractionFreeTest.java
@@ -1,11 +1,14 @@
 package org.wikipedia.distractionfree;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 import org.wikipedia.settings.Prefs;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(RobolectricTestRunner.class)
 public class DistractionFreeTest {
 
     @Test
@@ -14,13 +17,13 @@ public class DistractionFreeTest {
     }
 
     @Test
-    public void testIncognitoSetFalse() {
+    public void testDFSetFalse() {
         Prefs.disableDistractionFreeMode();
         assertFalse(Prefs.isWikiWalkingEnabled());
     }
 
     @Test
-    public void testIncognitoSetTrue()  {
+    public void testDFSetTrue() {
         Prefs.enableDistractionFreeMode();
         assertTrue(Prefs.isDistractionFreeModeEnabled());
     }
