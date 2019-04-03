675e17d483ca6d6f1acb27d7ba48affceb4e43b4
Philippe Lorange <philippe.lorange@gmail.com>
2019-04-02 21:50:38 -0400

    [#79] Added SpotifyReceiver to get real-time changes from Spotify

--- app/src/main/java/org/wikipedia/spotify/SpotifyRemote.java
+++ app/src/main/java/org/wikipedia/spotify/SpotifyRemote.java
@@ -8,20 +8,19 @@ import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.PlayerApi;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 
+/*
+*   This class will allow you to control the Spotify player (play, pause, seek, skip, etc).
+*   However, this is not where you will find the title, artist, album of the current song.
+*   Use SpotifyReceiver instead.
+*/
 public class SpotifyRemote {
-
-    private Callback callback;
-
-    public interface Callback {
-        void updateArtist(String artistName);
-    }
 
     private static final String CLIENT_ID = "89625714de2848f48e048a3f628968d5";
     private static final String REDIRECT_URI = "testtest://redirect";
     private SpotifyAppRemote mSpotifyAppRemote;
 
-    public SpotifyRemote(Fragment fragment, Callback callback) {
-        this.callback = callback;
+
+    public SpotifyRemote(Fragment fragment) {
         ConnectionParams connectionParams =
                 new ConnectionParams.Builder(CLIENT_ID)
                         .setRedirectUri(REDIRECT_URI)
@@ -48,10 +47,5 @@ public class SpotifyRemote {
         PlayerApi playerApi = mSpotifyAppRemote.getPlayerApi();
         playerApi.play(uri);
         Log.d("SpotifyRemote", "Song is playing");
-        mSpotifyAppRemote.getPlayerApi().getPlayerState().setResultCallback(playerState -> {
-            callback.updateArtist(playerState.track.artist.name);
-        }).setErrorCallback(throwable -> {
-
-        });
     }
 }
